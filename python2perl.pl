f73d1b03bf5ed5899026884b1b643cb2813c780c
Jothini Nellie Sivananthan <jnsi366@cse.unsw.EDU.AU>
2014-09-17 12:06:20 +1000

    starting point

--- python2perl.pl
+++ python2perl.pl
@@ -1,29 +1,47 @@
-#!/usr/bin/perl
-# Starting point for COMP2041/9041 assignment 
-# http://www.cse.unsw.edu.au/~cs2041/assignments/python2perl
-# written by andrewt@cse.unsw.edu.au September 2014
-
-while ($line = <>) {
-	if ($line =~ /^#!/ && $. == 1) {
-	
-		# translate #! line 
-		
+#!/usr/local/bin/perl -w
+while($line=<>){
+if ($line =~/^#!/ && $. == 1) {
 		print "#!/usr/bin/perl -w\n";
-	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-	
-		# Blank & comment lines can be passed unchanged
-		
-		print $line;
-	} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
-	
-		# Python's print print a new-line character by default
-		# so we need to add it explicitly to the Perl print statement
-		
-		print "print \"$1\\n\";\n";
-	} else {
-	
-		# Lines we can't translate are turned into comments
-		
-		print "#$line\n";
+} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+#Blank & comment lines can be passed unchanged
+#print $line;
+} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+print "print \"$1\\n\";\n";
+     } elsif($line =~m/hello world/){
+                print  "$line", "\n";
+    }elsif($line =~ s/^/\$/g && $line =~ s/$/\;/){
+	print "$line";
+     }elsif($line =~ m/\+/){
+	 print "$line\n";	
+	}elsif($line=~m/\-/){
+		print "$line";
+	}elsif($line=~m/\*/){
+		print "$line";
+        }elsif($line=~m/\//){
+        	print "$line";
+ 	}elsif($line=~m/\%/){
+		print "$line";
+	}elsif($line=m/\**/){
+		print "$line";
+        }elsif($line=~m/\>/){
+        	print "$line";
+        }elsif($line=~m/\</){
+        	print "$line";;
+        }elsif($line=~m/\!=/){
+        	print "$line";
+        }elsif($line=~m/\==/){
+        	print"$line";
+        }elsif($line=~m/\<>/){
+        	print "$line";
+        }elsif($line=~m/\>=/){
+        	print "$line";
+	}elsif($line=~m/\<=/){
+		print "$line";
+        }elsif($line=~m/\|/){
+		print "$line";
+        }elsif($line=~m/\^/){
+		print "$line\n";        
+        }else{
+               print "match not found\n";
 	}
 }
