b8cf6a44de04bd169dcaeb600209bd484b075259
Jothini Nellie Sivananthan <jnsi366@cse.unsw.EDU.AU>
2014-09-20 14:13:22 +1000

    modified code so that a closed bracket can be placed after the if and another bracket to replace the colon

--- python2perl.pl
+++ python2perl.pl
@@ -1,43 +1,29 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/perl
+# Starting point for COMP2041/9041 assignment 
+# http://www.cse.unsw.edu.au/~cs2041/assignments/python2perl
+# written by andrewt@cse.unsw.edu.au September 2014
 
-while($line=<>){
-$blank = 0;
-chomp($line);
-if ($line =~/^#!/ && $. == 1) {
-		$line =  "#!/usr/bin/perl -w";
-} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-#Blank & comment lines can be passed unchanged
-#print $line;
-$blank = 1;
-} elsif ($line =~ /^\s*print\s*"?(.*)"?\s*$/) {
-$line .= ";";
-$line =~ s/;/\,"\\n\";/;
-				
-    }elsif($line =~m/hello world/){
-                $line =~ s/;/,\"\\n\";/;
-		print "$line";
-    }elsif($line =~s/factor/\$factor/g){
-#	print $line =~ s/;/\,"\\n\";/;
+while ($line = <>) {
+	if ($line =~ /^#!/ && $. == 1) {
 	
-  }   
-	$line =~ s/answer/\$answer/;
-#	print $answer;
-  
-#	$line =~ s/\if$answer/if(\$answer/; 	
-#   }elsif($line =~ s/if answer/if(\$answer/g){
-#	print $line;
- 	if($line =~ /([0-9])|(\")$/){
-		print "$line;\n";
-	
-	}elsif ($line=~s/if /if(/){
-		print "$line;\n";
-	
-	}elsif($blank != 1){
-		print "$line\n";
-
-	}else{
-#		print "#$line\n";
-	}
-	
+		# translate #! line 
 		
+		print "#!/usr/bin/perl -w\n";
+	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+	
+		# Blank & comment lines can be passed unchanged
+		
+		print $line;
+	} elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+	
+		# Python's print print a new-line character by default
+		# so we need to add it explicitly to the Perl print statement
+		
+		print "print \"$1\\n\";\n";
+	} else {
+	
+		# Lines we can't translate are turned into comments
+		
+		print "#$line\n";
+	}
 }
