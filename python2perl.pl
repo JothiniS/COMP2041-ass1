d8d41d30165be7e1bb7af1c6a87002f4f6204e4f
Jothini Nellie Sivananthan <jnsi366@cse.unsw.EDU.AU>
2014-09-20 10:35:50 +1000

    testing with the test cases provided and modifiying code to test answer4

--- python2perl.pl
+++ python2perl.pl
@@ -7,7 +7,7 @@ if ($line =~/^#!/ && $. == 1) {
 		$line =  "#!/usr/bin/perl -w";
 } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 #Blank & comment lines can be passed unchanged
-print $line;
+#print $line;
 $blank = 1;
 } elsif ($line =~ /^\s*print\s*"?(.*)"?\s*$/) {
 $line .= ";";
@@ -16,23 +16,27 @@ $line =~ s/;/\,"\\n\";/;
     }elsif($line =~m/hello world/){
                 $line =~ s/;/,\"\\n\";/;
 		print "$line";
-    }elsif($line=~s/factor/\$factor/g){
-	#print $line =~ s/;/\,"\\n\";/;
-}
-
-	$line =~ s/answer/\$answer/;	
+    }elsif($line =~s/factor/\$factor/g){
+#	print $line =~ s/;/\,"\\n\";/;
 	
-	#print $line;
-	if($line =~ /([0-9])|(\")$/){
+  }   
+	$line =~ s/answer/\$answer/;
+#	print $answer;
+  
+#	$line =~ s/\if$answer/if(\$answer/; 	
+#   }elsif($line =~ s/if answer/if(\$answer/g){
+#	print $line;
+ 	if($line =~ /([0-9])|(\")$/){
 		print "$line;\n";
-	;
-	}elsif ($line=~s/if /if(/g){
+	
+	}elsif ($line=~s/if /if(/){
 		print "$line;\n";
 	
 	}elsif($blank != 1){
 		print "$line\n";
+
 	}else{
-		print "#$line\n";
+#		print "#$line\n";
 	}
 	
 		
