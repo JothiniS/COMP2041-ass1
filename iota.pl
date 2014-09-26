535771727c4f2be678a85e6b1eed84eadef27f73
Jothini Nellie Sivananthan <jnsi366@cse.unsw.EDU.AU>
2014-09-26 12:44:02 +1000

    worked on varibales in subset 1

--- iota.pl
+++ iota.pl
@@ -1,7 +1,3 @@
 #!/usr/bin/perl -w
-
-$x = 1;
-while ($x <= 10) {
-	print "$x\n";
-	$x = $x + 1;
-}
+$x=1;
+while( $x <= 10){ print $x; $x = $x + 1,"\n";};
--- python2perl.pl
+++ python2perl.pl
@@ -1,42 +1,79 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl 
 #By Jothini Sivananthan
 while($line=<>){
 $blank = 0;
 chomp($line);
 if ($line =~/^#!/ && $. == 1){
-
-	$line =~  "#!/usr/bin/perl -w";
-} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+$line="#\!/usr/bin/perl -w"; 
+} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/){
 #Blank & comment lines can be passed unchanged
 #print $line;
-$blank = 1;
+	$blank = 1;
 } elsif ($line =~ /^\s*print\s*"?(.*)"?\s*$/) {
-#$line .= ";";
-#$line =~ s/;/\,"\\n\";/;
+$line .= ";";
+$line =~ s/;/\,"\\n\";/;
 				
     }elsif($line =~m/hello world/){
                 $line =~ s/;/,\"\\n\";/;
 		print "$line";
+    }elsif($line =~ /print/){
+     $line.=",\"\\n\"";
 #    }elsif($line =~s/factor/\$factor/g){
-	#print $line =~ s/;/\,"\\n\";/;
+#	print $line =~ s/;/\,"\\n\";/;
+#	print $line;
+
+#$line=~s/^\ *[a-zA-Z_0-9]*\ *=/^\ *\$[a-zA-Z_0-9]*\ *=/g;
+     # print "$line";
+}elsif($line=~ /^\ *[a-zA-Z].*\ *=/){
+$line=~ s/\s*//g;
+@updatedArray = split(/\W/,$line);
+push (@words,$updatedArray[0]);
 }
-#	$line =~ s/factor/\$factor/;
-#elsif($line =~s/answer/\$answer/g&& $line =~ s/if answer/if\(\answer/g &&$line=~ s/\:/\)/g ){
-#}elsif( $line=~s/answer/\$answer/){
-  $line =~ s/answer/\$answer/; 
-if($line =~ s/ answer/\$answer/g &&  $line=~ s/\:/\)/g && $line =~ s/if /if\(/g && $line=~ s/\)/\)\{/g && $line =~/\;$/){
-}elsif($line=~/\;$/){
-#		print "}\n";
-#
-}elsif($line=~s/while /while\(/g && $line=~s/\)/\)\{/g && $line=~/\;$/){
-	print"}\n";
+$i=0;
+while($i<=$#words){
+
+	$line=~s/$words[$i]/\$$words[$i]/g;
+ 	$i++;
+}
+#$line=~s/var1/\$var1/g;
+# $line =~ s/answer/\$answer/;
+#$line=~ s/factor/\$factor/g; 
+
+if( $line=~ /^if/ &&  $line =~ /\:/ ) { 
+	$line =~ s/if/if\(/g;
+	$line=~s/\:/\)/g;
+	#$line=~s/[0-9]\:/\{/g;
+	$line=~ s/\)/\)\{/g;
+	#if($line=~/if$/ && $line=~/\d/){
+	
+	$line=~ s/$/\;\}/g;
+	#}
+	#$line=~s/\d$/\d\{/g;
+	#$line=~s/\:/\)/g;
+#}elsif( $line=~ s/\:/\)/g && $line =~ s/if /if\(/g && $line=~ s/\)/\)\{/g && $line =~/\;$/){
+#}elsif($line=~ /^if/ && $line=~/\;$/){
+#$line=~ s/\;$/\;\}/g;
+	#print "}\n";
+#}elsif($line=~s/if /if\(/g){
+
+}elsif($line=~ /^while/ && $line=~ /\:/ && $line=~/print/){
+$line=~s/while/while\(/g ;
+$line=~s/\:/\)/g;
+$line=~s/\)/\)\{/g ;
+$line=~ s/$/\;\}/g;
+#$line=~/\;$/){
+	#print"}\n";
 #}elsif($line=~s/factor/\$factor/g){
 #	print "$line";   
-}
+#}elsif($line=~s/var1/\$var1/g){
+#	print "$line";
+#}elsif($line=~s/var2/\$var2/g){
+	#print "$line";
 #	if($line =~ s/if answer/if\(\$answer/){
 #	}	 	
 #	print $line;
- 
+#}elsif($line=~ 
+}
  	if($line =~ /([0-9])|(\")$/){
 		print "$line;\n";
 	
