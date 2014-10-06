0282b6ce71df9cd4fff408280eb68fa2fe4fdc3a
Jothini Nellie Sivananthan <jnsi366@cse.unsw.EDU.AU>
2014-10-06 16:21:12 +1100

    modification to demo9 testfile

--- demo09.py
+++ demo09.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
 
-print "Good bye!"
+
+var1 = 100
+if var1:
+   print "1 - Got a true expression value"
+   print var1
+else:
+   print "1 - Got a false expression value"
+   print var1
+
